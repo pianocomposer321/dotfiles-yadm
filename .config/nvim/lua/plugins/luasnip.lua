e9343f27e7775b2f28e326185481849822241187
pianocomposer321 <tmarklebw88@gmail.com>
2023-06-11 16:29:55 -0500

    Plugins(snippets): Get luasnip working

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,7 +1,43 @@
 return {
   "L3MON4D3/LuaSnip",
+  dependencies = "honza/vim-snippets",
+  event = "VeryLazy",
   init = function()
-    vim.keymap.set({"i", "s"}, "<C-j>", function() require("luasnip").jump(1) end)
+    vim.keymap.set({"i", "s"}, "<C-j>", function() require("luasnip").expand_or_jump(1) end)
     vim.keymap.set({"i", "s"}, "<C-k>", function() require("luasnip").jump(-1) end)
+
+    vim.api.nvim_create_user_command("LuaSnipEdit", function()
+      vim.cmd.edit(string.format("%s/%s.snippets", "~/.config/nvim/snippets", vim.o.ft))
+    end, {})
+
+    vim.api.nvim_create_user_command("LuaSnipEditLocal", function()
+      if vim.fn.isdirectory("snippets") == 0 then
+        vim.fn.mkdir("snippets")
+      end
+
+      vim.cmd.edit(string.format("snippets/%s.snippets", vim.o.ft))
+    end, {})
+
+    -- vim.api.nvim_create_user_command("LuaSnipEdit", function()
+    --   -- require("luasnip.loaders").edit_snippet_files {}
+    --   require("luasnip.loaders").edit_snippet_files {
+    --     extend = function(ft, paths)
+    --       -- if #paths == 0 then
+    --         return {
+    --           { "$CONFIG/" .. ft .. ".snippets",
+    --           string.format("%s/%s.snippets", "~/.config/nvim/snippets", ft) }
+    --         }
+    --       -- end
+    --
+    --       -- return {}
+    --     end
+    --   }
+    -- end, {})
+
   end,
+  config = function()
+    local paths = vim.api.nvim_get_runtime_file("snippets", true)
+    table.insert(paths, vim.fn.getcwd() .. "/snippets")
+    require("luasnip.loaders.from_snipmate").lazy_load({paths = paths})
+  end
 }
