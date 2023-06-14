58b8142ae28ceae623d8fe682f6f3902010ee9ce
pianocomposer321 <tmarklebw88@gmail.com>
2023-06-13 21:55:18 -0500

    Install gitsigns.nvim

--- .config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  -- lazy = false,
-  event = "VeryLazy",
-  init = function()
-    vim.keymap.set("n", "<LEADER>gg", vim.cmd.G)
-    vim.keymap.set("n", "<LEADER>gc", function() vim.cmd.G("commit") end)
-  end
-}
---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("gitsigns").setup {}
+    end
+  },
+  {
+    "tpope/vim-fugitive",
+    -- lazy = false,
+    event = "VeryLazy",
+    init = function()
+      vim.keymap.set("n", "<LEADER>gg", vim.cmd.G)
+      vim.keymap.set("n", "<LEADER>gc", function() vim.cmd.G("commit") end)
+    end
+  }
+}
