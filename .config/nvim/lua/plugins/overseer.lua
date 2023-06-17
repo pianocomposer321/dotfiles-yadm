e82413fbea2eb841332e912bc7170cb8c9d03dc6
pianocomposer321 <tmarklebw88@gmail.com>
2023-06-16 20:22:15 -0500

    refactor overseer

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,22 +1,29 @@
-local function restart_last()
-  local tasks = require("overseer").list_tasks {recent_first = true}
-  if vim.tbl_isempty(tasks) then
-    vim.notify("No tasks found", vim.log.levels.WARN)
-  else
-    require("overseer").run_action(tasks[1], "restart")
-  end
-end
-
 return {
   {
     "stevearc/overseer.nvim",
     dev = true,
     cmd = "Make",
     init = function()
-      vim.keymap.set("n", "<LEADER>ro", function() require("overseer").toggle() end)
-      vim.keymap.set("n", "<LEADER>rr", function() require("overseer").run_template { prompt = "avoid" } end)
+      local overseer = require("overseer")
+
+      local function get_last()
+        local tasks = overseer.list_tasks {recent_first = true}
+        if vim.tbl_isempty(tasks) then
+          vim.notify("No tasks found", vim.log.levels.WARN)
+          return false
+        else
+          return tasks[1]
+        end
+      end
+
+      local function restart_last()
+        overseer.run_action(get_last(), "restart")
+      end
+
+      vim.keymap.set("n", "<LEADER>ro", function() overseer.toggle() end)
+      vim.keymap.set("n", "<LEADER>rr", function() overseer.run_template { prompt = "avoid" } end)
       vim.keymap.set("n", "<LEADER><CR>", restart_last)
-      vim.keymap.set("n", "<LEADER>ra", function() require("overseer").commands.task_action() end)
+      vim.keymap.set("n", "<LEADER>ra", function() overseer.commands.task_action() end)
     end,
     config = function()
       local overseer = require("overseer")
