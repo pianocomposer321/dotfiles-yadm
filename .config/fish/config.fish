2df14c673cf21845a11cd887ce92a0299aa1c5b3
pianocomposer321 <tmarklebw88@gmail.com>
2023-09-01 16:59:18 -0500

    Haskell stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/composer3/.ghcup/bin $PATH # ghcup-env
