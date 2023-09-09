0eba5dc26cec2fdc08bbfcee49382d39dacfe570
pianocomposer321 <tmarklebw88@gmail.com>
2023-09-09 14:20:29 -0500

    Add tests to see if starship and zoxide are installed in fish config
    This way if I download my dotfiles to a new system it won't break if
    these programs aren't yet installed.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    starship init fish | source
-    zoxide init fish | source
+    if command -q starship 
+        starship init fish | source
+    end
+    if command -q zoxide
+        zoxide init fish | source
+    end
 end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/composer3/.ghcup/bin $PATH # ghcup-env
