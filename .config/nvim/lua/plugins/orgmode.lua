a4b42d87ae624df0982233771f4d32424b293dc9
pianocomposer321 <tmarklebw88@gmail.com>
2023-12-09 16:07:27 -0600

    Change org-mode notes directory

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -6,8 +6,8 @@ return {
     config = function()
       require("orgmode").setup_ts_grammar()
       require("orgmode").setup {
-        org_agenda_files = {"~/Documents/notes/**/*"},
-        org_default_notes_file = "~/Documents/notes/refile.org",
+        org_agenda_files = {"~/Documents/notes-org/**/*"},
+        org_default_notes_file = "~/Documents/notes-org/refile.org",
       }
     end
   },
