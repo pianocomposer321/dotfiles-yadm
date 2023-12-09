cc1a010d5a46872c28209a31efc5973bb4685b39
pianocomposer321 <tmarklebw88@gmail.com>
2023-12-09 15:59:53 -0600

    Add globals file with reload functions
    These functions remove the cached lua modules so thay can be loaded with
    state changes applied. Useful for developing plugins or configs.

---
+++ .config/nvim/lua/user/globals.lua
@@ -0,0 +1,15 @@
+RELOAD = function(...)
+  local reloader = require
+
+  local ok, plenary_reload = pcall(require, "plenary.reload")
+  if ok then
+    reloader = plenary_reload.reload_module
+  end
+
+  return reloader(...)
+end
+
+R = function(name)
+  RELOAD(name)
+  return require(name)
+end
