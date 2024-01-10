4b4b780ac075147b67971d1ff4cbe1a174210f79
pianocomposer321 <tmarklebw88@gmail.com>
2024-01-09 22:27:20 -0600

    Separate out overseer-dispatch into its own plugin

--- .config/nvim/lua/plugins/dispatch.lua
+++ .config/nvim/lua/plugins/dispatch.lua
@@ -1,5 +1,19 @@
 return {
-  "tpope/vim-dispatch",
-  enabled = false,
-  event = "VeryLazy"
+  {
+    "tpope/vim-dispatch",
+    enabled = false,
+    event = "VeryLazy"
+  },
+  {
+    "pianocomposer321/dispatch-overseer.nvim",
+    dependencies = { "stevearc/overseer.nvim" },
+    dev = true,
+    event = "VeryLazy",
+    config = function()
+      require("dispatch-overseer").setup {
+        create_mappings = true,
+        additional_components = { "user.track_history" },
+      }
+    end,
+  },
 }
--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,10 +1,17 @@
+
 return {
   {
     "stevearc/overseer.nvim",
     -- dev = true,
     cmd = {"Make", "Run"},
     init = function()
-      require("user.overseer_dispatch").setup_mappings()
+      local util = require("user.overseer_util")
+      local overseer = require("overseer")
+      vim.keymap.set("n", "<LEADER>ro", function() overseer.toggle() end)
+      vim.keymap.set("n", "<LEADER>rr", function() overseer.run_template { prompt = "avoid" } end)
+      vim.keymap.set("n", "<LEADER><CR>", util.restart_last_task)
+      vim.keymap.set("n", "<LEADER>ra", function() overseer.commands.task_action() end)
+      vim.keymap.set("n", "<LEADER>rv", util.open_vsplit_last)
     end,
     config = function()
       local overseer = require("overseer")
@@ -18,8 +25,6 @@ return {
       }, function(task_defn, a_util)
         a_util.add_component(task_defn, { "unique" })
       end)
-
-      require("user.overseer_dispatch").setup_commands()
     end
   },
 }
