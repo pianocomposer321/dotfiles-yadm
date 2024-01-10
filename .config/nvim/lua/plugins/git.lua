7f632ed690e9c3f0232b2056db801a2aa05a906f
pianocomposer321 <tmarklebw88@gmail.com>
2024-01-09 22:26:25 -0600

    Add mappings for git push and general git commands

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -13,6 +13,8 @@ return {
     init = function()
       vim.keymap.set("n", "<LEADER>gg", vim.cmd.G)
       vim.keymap.set("n", "<LEADER>gc", function() vim.cmd.G("commit") end)
+      vim.keymap.set("n", "<LEADER>gp", function() vim.cmd.G("push") end)
+      vim.keymap.set("n", "<LEADER>g<SPACE>", ":Git ")
     end
   }
 }
